2bba9906fe059cbe95ea22fb453a0f8091cff320
Mary <lyamooore@mail.ru>
2026-01-22 04:45:01 +0300

    refactor: replace inline styles with styled components

---
+++ src/pages/users/UsersPage.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Page = styled.div`
+  max-width: 900px;
+  margin: 40px auto;
+  padding: 0 24px 40px;
+`
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 24px;
+`
+
+export const Footer = styled.div`
+  margin-top: 24px;
+`
